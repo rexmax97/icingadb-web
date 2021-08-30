0ce302db451a4787304af344f072ab6ecadc1781
rexmax97 <rexmax97@users.noreply.github.com>
2021-08-30 12:42:51 -0500

    Update run.php

--- run.php
+++ run.php
@@ -1,19 +1,36 @@
 <?php
 
-/* Icinga DB Web | (c) 2020 Icinga GmbH | GPLv2 */
-
 /** @var $this \Icinga\Application\Modules\Module */
 
-$this->provideHook('ApplicationState');
-$this->provideHook('X509/Sni');
-$this->provideHook('health', 'IcingaHealth');
-$this->provideHook('health', 'RedisHealth');
+use Icinga\Application\Icinga;
+use Icinga\Application\Modules\Manager;
+use Icinga\Application\Modules\Module;
 
-if (! $this->app->getModuleManager()->hasEnabled('monitoring')) {
-    // Ensure we can load some classes/interfaces for compatibility with legacy hooks
-    $this->app->getLoader()->registerNamespace(
-        'Icinga\\Module\\Monitoring',
-        $this->getLibDir() . '/Monitoring',
-        $this->getApplicationDir()
-    );
+ $this->provideHook('ApplicationState');
+ $this->provideHook('X509/Sni');
+ $this->provideHook('health', 'IcingaHealth');
+ $this->provideHook('health', 'RedisHealth');
+
+$mm = Icinga::app()->getModuleManager();
+if (! $mm->hasEnabled('monitoring')) {
+    $monitoring = new Module(Icinga::app(), 'monitoring', $mm->getModuleDir('monitoring'));
+
+    $launchedConfigScript = new ReflectionProperty(Module::class,'triedToLaunchConfigScript');
+    $launchedConfigScript->setAccessible(true);
+    $launchedConfigScript->setValue($monitoring, true);
+
+    $registerAutoloader = new ReflectionMethod(Module::class, 'registerAutoloader');
+    $registerAutoloader->setAccessible(true);
+    $registerAutoloader->invoke($monitoring);
+
+    $registered = new ReflectionProperty(Module::class, 'registered');
+    $registered->setAccessible(true);
+    $registered->setValue($monitoring, true);
+
+    $loadedModules = new ReflectionProperty(Manager::class, 'loadedModules');
+    $loadedModules->setAccessible(true);
+    $loadedModules->setValue($mm, $loadedModules->getValue($mm) + ['monitoring' => $monitoring]);
+
+    $this->provideNavigationItem('host-action', $this->translate('Host Action'));
+    $this->provideNavigationItem('service-action', $this->translate('Service Action'));
 }
