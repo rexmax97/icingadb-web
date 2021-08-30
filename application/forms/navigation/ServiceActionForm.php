9b7204b92bdfeb36925d889a0c642950d98cc3b1
Yonas Habteab <yonas.habteab@netways.de>
2021-08-30 17:34:50 +0200

    Introduce ServiceAction navigation config form

---
+++ application/forms/navigation/ServiceActionForm.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Icinga DB Web | (c) 2021 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Icingadb\Forms\Navigation;
+
+class ServiceActionForm extends ActionForm
+{
+    protected $restrictions = 'icingadb/filter/services';
+}
