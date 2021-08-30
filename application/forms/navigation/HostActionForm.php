c9592f989205e7c51246e0c492af5c9792c11e75
Yonas Habteab <yonas.habteab@netways.de>
2021-08-30 17:34:00 +0200

    Introduce HostAction navigation form

---
+++ application/forms/navigation/HostActionForm.php
@@ -0,0 +1,10 @@
+<?php
+
+/* Icinga DB Web | (c) 2021 Icinga GmbH | GPLv2 */
+
+namespace Icinga\Module\Icingadb\Forms\Navigation;
+
+class HostActionForm extends ActionForm
+{
+    protected $restrictions = 'icingadb/filter/hosts';
+}
